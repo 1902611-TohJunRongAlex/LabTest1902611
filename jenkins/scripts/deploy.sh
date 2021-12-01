96368fddf09d74bc0059fc7e7c046532f033ef67
1902611-TohJunRongAlex <71168313+1902611-TohJunRongAlex@users.noreply.github.com>
2021-12-01 16:20:47 +0800

    Selenium

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /root/repo/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app --volume /root/repo/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
