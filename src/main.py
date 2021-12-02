72ded3f3e9238f2337f1978cfbe3bd7faa434af4
AlexTJR <1902611@sit.singaporetech.edu.sg>
2021-12-02 14:26:11 +0800

    tabs

--- src/main.py
+++ src/main.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
-    if request.method == 'POST':
-        searchvalue = request.form['search']
-        return render_template('done.html', searchvalue=searchvalue)
-    else:
-        return render_template('index.html')
+	if request.method == 'POST':
+		searchvalue = request.form['search']
+		return render_template('done.html', searchvalue=searchvalue)
+	else:
+		return render_template('index.html')
 
 
 @app.route("/done", methods=['GET', 'POST'])
 def done():
-    return render_template('done.html')
+	return render_template('done.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+	app.run(host="0.0.0.0", port=80)
