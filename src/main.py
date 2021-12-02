0154efe03970fafc7eeb2a6dc97613f202cda32b
AlexTJR <1902611@sit.singaporetech.edu.sg>
2021-12-02 14:21:53 +0800

    fix

--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-
 @app.route("/", methods=['GET', 'POST'])
 def main():
-	if request.method == 'POST':
+    if request.method == 'POST':
         searchvalue = request.form['search']
-		return render_template('done.html',searchvalue = searchvalue)
-    return render_template('index.html')
-	
+        return render_template('done.html', searchvalue=searchvalue)
+    else:
+        return render_template('index.html')
+
+
 @app.route("/done", methods=['GET', 'POST'])
 def done():
     return render_template('done.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port = 80)
+    app.run(host="0.0.0.0", port=80)
